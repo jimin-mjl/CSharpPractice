4acc72c002122c6d99b1afb93fd6ab257b628e11
jimin-mjl <jiminaleejung@gmail.com>
2022-12-02 21:15:19 +0900

    Feat: 별찍기 프로그램 작성

---
+++ 20221202-2.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void PrintStars(int num)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--별 찍기--");
+        Console.WriteLine();
+        Program.PrintStars(10);
+    }
+}
