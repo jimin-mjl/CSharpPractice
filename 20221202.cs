e5de1f3382c97738774f5e549a75b451dff173f9
jimin-mjl <jiminaleejung@gmail.com>
2022-12-02 21:04:13 +0900

    Feat: 구구단 출력 프로그램 만들어보기

---
+++ 20221202.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void PrintMulTable(int num)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            Console.WriteLine($"{num} * {i + 1} = {num * (i + 1)}");
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--구구단 출력하기--");
+        Console.WriteLine();
+
+        for (int i = 0; i < 9; i++)
+        {
+            Program.PrintMulTable(i + 1);
+        }
+    }
+}
